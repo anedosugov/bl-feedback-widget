45be32531ac5e5b1cdf870f2eebf57433d83f08e
anedosugov <anedosugov@gmail.com>
2019-03-28 13:30:49 +0300

    footer style fixup

--- src/components/FeedbackFooter.vue
+++ src/components/FeedbackFooter.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="feedback-footer">
-    <div>
-      <font-awesome-icon icon="heart"></font-awesome-icon>
-      <div>{{ votes }}</div>
-    </div>
-    <div>
-      <font-awesome-icon icon="comment"></font-awesome-icon>
-      <div>{{ answers }}</div>
+    <div class="feedback-footer__counters">
+      <div>
+        <font-awesome-icon icon="heart"></font-awesome-icon>
+        <div>{{ votes }}</div>
+      </div>
+      <div>
+        <font-awesome-icon icon="comment"></font-awesome-icon>
+        <div>{{ answers }}</div>
+      </div>
     </div>
     <div>{{ dateSince }}</div>
     <div>
@@ -27,12 +29,15 @@
   line-height: 1;
   font-size: 0.9rem;
   * {
+    cursor: pointer;
     margin-right: 0.5rem;
     display: inline-block;
   }
-  > * {
-    cursor: pointer;
-    margin-right: 0.8rem;
+  &__counters {
+    @extend .d-block;
+    @extend .d-sm-inline;
+    @extend .mb-2;
+    @extend .mb-sm-0;
   }
 }
 </style>
